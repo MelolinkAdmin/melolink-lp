29f0de71aa7cb40288289bccc6138f982df38dd2
Thiago Militão <thiago.militao2007@gmail.com>
2026-02-26 22:27:25 -0300

    fix: add reviews

--- app/src/components/sections/Reviews/ReviewsData.tsx
+++ app/src/components/sections/Reviews/ReviewsData.tsx
@@ -34,5 +34,33 @@ export const Reviews = [
         rating: 5,
         initial: "A",
         color: "bg-rose-600"
-    }
+    },
+    {
+        id: 5,
+        name: "Francismarlon Santos",
+        date: "Google Avaliações",
+        comment: "Excelência da equipe Melolink, desde o atendimento até a finalização do serviço. Destaque tbem ao pessoal do suporte do Whatsapp. Sempre educados e transparentes. Estão de parabéns!",
+        rating: 5,
+        initial: "F",
+        color: "bg-rose-600"
+    },
+    {
+        id: 6,
+        name: "Raisa Moraes",
+        date: "Google Avaliações",
+        comment: "Atendimento excelente! Sempre que tenho algum problema com queda da internet devido a cabos na rua, os técnicos se deslocam para o local, solucionando o problema de forma rápida. Obrigada pela atenção e suporte!",
+        rating: 5,
+        initial: "R",
+        color: "bg-rose-600"
+    },
+    {
+        id: 7,
+        name: "Rodrigo Bernardes",
+        date: "Google Avaliações",
+        comment: "Gostaria de elogiar a equipe de instalacao da melolink pela pontualidade deles além da atenção que tiveram comigo foram muito prestativos e rápidos na execução da instalação. Foi tudo perfeito os dois que vieram sao muito bons e merecem elogios",
+        rating: 5,
+        initial: "R",
+        color: "bg-rose-600"
+    },
+
 ];
