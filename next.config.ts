5289e5d50d969fe8cae0638f9fd83763a16c1c37
Thiago Militão <thiago.militao2007@gmail.com>
2026-03-03 17:32:58 -0300

    feat: redirect /admin to netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        // Quando alguém acessar melolink.com.br/admin
+        source: '/admin',
+        // Ele será levado para o link do Netlify que funciona
+        destination: 'https://melolink.netlify.app/admin/index.html',
+        // 'permanent: true' diz aos buscadores que esse é o caminho definitivo (status 301)
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
