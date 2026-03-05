039e2bb88b754d6692ea63c724cd8bc0ffab200e
Thiago Militão <thiago.militao2007@gmail.com>
2026-03-05 11:38:36 -0300

    fix: decap

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        // Quando alguém acessar melolink.com.br/admin
-        source: '/admin',
-        // Ele será levado para o link do Netlify que funciona
-        destination: 'https://melolink.netlify.app/admin/index.html',
-        // 'permanent: true' diz aos buscadores que esse é o caminho definitivo (status 301)
-        permanent: true,
-      },
-    ];
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
